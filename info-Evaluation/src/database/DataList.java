b1ec1e2c7906a9bc7f8d5615603cc643474ce0a1
Ward Mattar <wardm@campus.technion.ac.il>
2016-12-05 18:49:25 +0200

    Fixed settings

--- info-Evaluation/src/database/DataList.java
+++ info-Evaluation/src/database/DataList.java
@@ -1,31 +1,36 @@
 package database;
 
 import java.util.ArrayList;
+
 /**
  * 
- * @author netanel
- * This class holds all the data to be shown in the table in the GUI
+ * @author netanel This class holds all the data to be shown in the table in the
+ *         GUI
  */
-public class DataList{
+public class DataList {
 	private ArrayList<tableTuple> list;
 	private int numOfTuples;
-	
-	public DataList(){
-		this.list= new ArrayList<>();
-		this.numOfTuples=0;
+
+	public DataList() {
+		this.list = new ArrayList<>();
+		this.numOfTuples = 0;
 	}
-	public void insert(String name, String date, String reason){
-		this.list.add(new tableTuple(name,date,reason));
+
+	public void insert(String name, String date, String reason) {
+		this.list.add(new tableTuple(name, date, reason));
 		++this.numOfTuples;
 	}
-	public void insert(tableTuple ¢){
+
+	public void insert(tableTuple ¢) {
 		this.list.add(¢);
 		++this.numOfTuples;
 	}
-	public int getNumOfTuples(){
+
+	public int getNumOfTuples() {
 		return this.numOfTuples;
 	}
-	public ArrayList<tableTuple> getList(){
+
+	public ArrayList<tableTuple> getList() {
 		return this.list;
 	}
 	// if needed, we can implement here sorts, refines etc.
