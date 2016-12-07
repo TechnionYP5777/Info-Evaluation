4b569f82d945d30e4ffef26b11564e0cb8d51cf5
Ward Mattar <wardm@campus.technion.ac.il>
2016-12-07 23:28:12 +0200

    #15: added RefineTable class @ward-mattar

---
+++ info-Evaluation/src/guiFrames/GUI.java
@@ -0,0 +1,108 @@
+package guiFrames;
+
+import java.awt.EventQueue;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.border.LineBorder;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+
+/**
+ * This class implements our project GUI
+ * 
+ * @author Vivian Shehadeh
+ */
+public class GUI {
+
+	private JFrame frame;
+	private JTextField searchTxt;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					GUI window = new GUI();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public GUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setTitle("Info Evaluation");
+		frame.setResizable(false);
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		JMenuBar menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+
+		JMenu mnFile = new JMenu("File");
+		menuBar.add(mnFile);
+
+		JMenu mnEdit = new JMenu("Edit");
+		menuBar.add(mnEdit);
+
+		JMenu mnHelp = new JMenu("Help");
+		menuBar.add(mnHelp);
+
+		JMenuItem mntmHelp = new JMenuItem("Help");
+		mnHelp.add(mntmHelp);
+
+		JButton btnSearch = new JButton("Search");
+
+		searchTxt = new JTextField();
+		searchTxt.setText("Search");
+		searchTxt.setColumns(10);
+		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
+		groupLayout.setHorizontalGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+				.addGroup(groupLayout.createSequentialGroup().addContainerGap()
+						.addComponent(searchTxt, GroupLayout.PREFERRED_SIZE, 281, GroupLayout.PREFERRED_SIZE)
+						.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+						.addComponent(btnSearch).addGap(74)));
+		groupLayout.setVerticalGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup().addGap(20)
+						.addGroup(groupLayout.createParallelGroup(Alignment.LEADING, false)
+								.addComponent(searchTxt, Alignment.TRAILING).addComponent(btnSearch, Alignment.TRAILING,
+										GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+						.addContainerGap(234, Short.MAX_VALUE)));
+		frame.getContentPane().setLayout(groupLayout);
+
+		table = new JTable();
+		table.setShowVerticalLines(false);
+		table.setCellSelectionEnabled(true);
+		table.setColumnSelectionAllowed(true);
+		table.setBorder(new LineBorder(null));
+		for (int count = 1; count <= 10; ++count)
+			table.setModel(new DefaultTableModel(new Object[][] { { "Name", "Date", "Reason" } },
+					new String[] { "name", "date", "Reason" }));
+		table.setBounds(30, 80, 250, 180);
+		frame.getContentPane().add(table);
+	}
+}
---
+++ info-Evaluation/src/guiFrames/RefineTable.java
@@ -0,0 +1,85 @@
+package guiFrames;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.swing.table.DefaultTableModel;
+
+import java.util.ArrayList;
+
+/**
+ * This class implements list refinements on the GUI event table
+ * 
+ * @author Ward Mattar
+ */
+public class RefineTable {
+	private ArrayList<String> fields;
+
+	public ArrayList<String> getFeilds() {
+		return fields;
+	}
+
+	public void addFeild(String name) {
+		if (name != null && !"".equals(name))
+			fields.add(name);
+	}
+
+	public void removeFeild(String name) {
+		if (fieldExist(name))
+			fields.remove(name);
+	}
+
+	public boolean fieldExist(String name) {
+		return name != null && !"".equals(name) && fields.contains(name);
+	}
+
+	/**
+	 *
+	 * accepts a JTable and list of events as a SQL query result and fills them
+	 * in table
+	 * 
+	 * @throws SQLException
+	 */
+	public void fillEventsTable(DefaultTableModel m, ResultSet s) throws SQLException {
+		if (m == null || s == null)
+			return;
+		for (int ¢ = 0; ¢ < m.getRowCount(); ++¢)
+			m.removeRow(¢);
+		for (Object tempEvent[] = new Object[3]; s.next();) {
+			tempEvent[0] = s.getObject("Name");
+			tempEvent[1] = s.getObject("Date");
+			tempEvent[2] = s.getObject("Reason");
+			m.addRow(tempEvent);
+		}
+	}
+
+	/**
+	 * accepts a JTable and sorts the content of the table according to the
+	 * given field name
+	 */
+	public void sortBy(DefaultTableModel m, String fieldName) {
+		if (m == null || !fieldExist(fieldName))
+			return;
+		@SuppressWarnings("unused")
+		ArrayList<String> events;
+		switch (fieldName) {
+		case "Date":
+			// TODO: call fillTable with
+			// the result of runQuery("SELECT * FROM events ORDER BY UNIX_TIMESTAMP(date) DESC")
+			break;
+		case "Name":
+			// TODO: call fillTable with
+			// the result of runQuery("SELECT * FROM events ORDER BY name DESC")
+			break;
+		case "Reason":
+			// TODO: call fillTable with
+			// the result of runQuery("SELECT * FROM events ORDER BY reason")
+			break;
+		}
+
+	}
+
+	public RefineTable() {
+		fields = new ArrayList<String>();
+	}
+}
