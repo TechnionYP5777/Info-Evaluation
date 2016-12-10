73c22f06895cc99e46140bb6bbb30be7e9842631
geniashand <genia.shandalov@gmail.com>
2016-12-10 19:04:39 +0200

    AnalyzePage class and test class added. #20

---
+++ src/main/Analyze/AnalyzePage.java
@@ -0,0 +1,5 @@
+package main.Analyze;
+
+public class AnalyzePage {
+
+}
---
+++ src/test/Analyze/AnalyzePageTest.java
@@ -0,0 +1,15 @@
+package test.Analyze;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import main.Analyze.*;
+
+public class AnalyzePageTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
