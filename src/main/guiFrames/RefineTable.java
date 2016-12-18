4fde5a6806fd0d3ae6137a423141d5b1ce104d39
Ward Mattar <wardm@campus.technion.ac.il>
2016-12-18 19:00:07 +0200

    added filter support

--- src/main/guiFrames/RefineTable.java
+++ src/main/guiFrames/RefineTable.java
@@ -15,6 +15,9 @@ import java.util.ArrayList;
  * @author Ward Mattar
  */
 public class RefineTable {
+	public RefineTable() {
+		fields = new ArrayList<String>();
+	}
 	private ArrayList<String> fields;
 
 	public ArrayList<String> getFields() {
@@ -48,9 +51,9 @@ public class RefineTable {
 		for (int ¢ = 0; ¢ < m.getRowCount(); ++¢)
 			m.removeRow(¢);
 		for (Object tempEvent[] = new Object[3]; s.next();) {
-			tempEvent[0] = s.getObject("Name");
-			tempEvent[1] = s.getObject("Date");
-			tempEvent[2] = s.getObject("Reason");
+			tempEvent[0] = s.getObject("name");
+			tempEvent[1] = s.getObject("arrest_date");
+			tempEvent[2] = s.getObject("reason");
 			m.addRow(tempEvent);
 		}
 	}
@@ -62,23 +65,21 @@ public class RefineTable {
 	public void sortBy(MySQLConnector c, DefaultTableModel m, String fieldName) throws SQLException {
 		if (m == null || !fieldExist(fieldName))
 			return;
-		@SuppressWarnings("unused")
-		ArrayList<String> events;
 		try{
 			ResultSet r;
 			switch (fieldName) {
 				case "Date":
-					r = c.runQuery("SELECT * FROM celebsArrests ORDER BY UNIX_TIMESTAMP(date) DESC");
+					r = c.runQuery("SELECT * FROM celebs_arrests ORDER BY UNIX_TIMESTAMP(arrest_date) DESC");
 					fillEventsTable(m,r); 
 					r.close();
 					break;
 				case "Name":
-					r = c.runQuery("SELECT * FROM celebsArrests ORDER BY name DESC");
+					r = c.runQuery("SELECT * FROM celebs_arrests ORDER BY name");
 					fillEventsTable(m,r); 					
 					r.close();					
 					break;
 				case "Reason":
-					r = c.runQuery("SELECT * FROM celebsArrests ORDER BY reason");
+					r = c.runQuery("SELECT * FROM celebs_arrests ORDER BY reason");
 					fillEventsTable(m,r); 					
 					r.close();
 					break;	
@@ -91,21 +92,89 @@ public class RefineTable {
 	/**
 	 * filters the content of the events table according to the given field name
 	 * and value
+	 * @throws SQLException 
 	 */
-	public void filterBy(DefaultTableModel m, String fieldName, String fieldValue) {
+	public void filterBy(MySQLConnector c, DefaultTableModel m, String fieldName, String fieldValue) throws SQLException {
 		if (m != null && fieldExist(fieldName) && fieldValue != null)
-			switch (fieldName) {
-			case "Date":
-				break;
-			case "Name":
-				break;
-			case "Reason":
-				break;
+			try{
+				ResultSet r;
+				switch (fieldName) {
+					case "Date":
+						r = c.runQuery("SELECT * FROM celebs_arrests WHERE (YEAR(arrest_date) = " + fieldValue + " ) ORDER BY arrest_date DESC");
+						fillEventsTable(m,r); 
+						r.close();
+						break;
+					case "Name":
+						r = c.runQuery("SELECT * FROM celebs_arrests WHERE name LIKE \"%" + fieldValue + "%\" ORDER BY name");
+						fillEventsTable(m,r); 					
+						r.close();					
+						break;
+					case "Reason":
+						r = c.runQuery("SELECT * FROM celebs_arrests WHERE reason LIKE \"%" + fieldValue + "%\" ORDER BY reason");
+						fillEventsTable(m,r); 					
+						r.close();
+						break;	
+				}
+			}catch(SQLException e){
+				throw e;
 			}
-
 	}
 
-	public RefineTable() {
-		fields = new ArrayList<String>();
+
+
+	/**
+	 *
+	 * accepts a ListArray<String> representing a drop menu fills them
+	 * with the category option list
+	 * @throws SQLException
+	 */
+	public void fillMenu(ArrayList<String> m, String categoryName, ResultSet s) throws SQLException {
+		if (m == null || s == null)
+			return;
+		for (String temp = "N/A"; s.next();) {
+			switch(categoryName) {
+				case "Date":
+					temp = s.getString("arrest_date");
+					break;
+				case "Name":
+					temp = s.getString("name");					
+					break;
+				case "Reason":
+					temp = s.getString("reason");
+					break;	
+			}
+			m.add(new String(temp));
+		}
 	}
+	
+	/**
+	 * fills a drop down menu list with a specific category values
+	 * @throws SQLException
+	 **/
+	public void getCategory(MySQLConnector c, DefaultTableModel m, String categoryName) throws SQLException {
+		if (m != null && fieldExist(categoryName))
+			try{
+				ResultSet r;
+				switch (categoryName) {
+					case "Date":
+						r = c.runQuery("SELECT YEAR(arrest_date) FROM celebs_arrests ORDER BY arrest_date DESC, name, reason");
+						fillEventsTable(m,r); 
+						r.close();
+						break;
+					case "Name":
+						r = c.runQuery("SELECT name FROM celebs_arrests ORDER BY name, arrest_date DESC, reason");
+						fillEventsTable(m,r); 					
+						r.close();					
+						break;
+					case "Reason":
+						r = c.runQuery("SELECT reason FROM celebs_arrests ORDER BY reason, name, arrest_date DESC");
+						fillEventsTable(m,r); 					
+						r.close();
+						break;	
+				}
+			}catch(SQLException e){
+				throw e;
+			}
+	}
+		
 }
