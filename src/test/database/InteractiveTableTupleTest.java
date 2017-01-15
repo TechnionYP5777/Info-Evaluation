915adf07e885638e48c3a0a26edffe30adf807f9
NetanelFelcher <netanel_felcher@hotmail.com>
2017-01-15 11:37:15 +0200

    added test for key words #86

--- src/test/database/InteractiveTableTupleTest.java
+++ src/test/database/InteractiveTableTupleTest.java
@@ -45,5 +45,23 @@ public class InteractiveTableTupleTest{
 		t.setReasons(lst);
 		assertEquals(t.getReasons().get(0).getProbability(), 0.4, 0);
 		assertEquals(t.getReasons().get(0).getReason(), "stubbing");
+	}	
+	@Test public void test3(){
+		ReasonPair rp=new ReasonPair();
+		rp.setProbability(0.4);
+		rp.setReason("stubbing");
+		LinkedList<ReasonPair> lst= new LinkedList<>();
+		lst.add(rp);
+		InteractiveTableTuple t=new InteractiveTableTuple("Justin Bieber", "12/02/2016", lst);
+		assertEquals(t.getDate(), "12/02/2016");
+		assertEquals(t.getName(), "Justin Bieber");
+		assertEquals((t.getRegularDate() + ""), "Fri Dec 02 00:00:00 IST 2016");
+		assertTrue(t.getKeyWords().isEmpty());
+		assertEquals(t.getReasons().get(0).getProbability(), 0.4, 0);
+		assertEquals(t.getReasons().get(0).getReason(), "stubbing");
+		t.addKeyWord("stubbing");
+		assertEquals(t.getKeyWords().get(0), "stubbing");
+		t.addKeyWord("arrested");
+		assertEquals(t.getKeyWords().get(1), "arrested");
 	}		
 }
