e807967dd6a59fa86129980f0ab4493c8409afc1
NetanelFelcher <netanel_felcher@hotmail.com>
2017-01-15 11:33:56 +0200

    added test with a list of reasonPair #86

--- src/test/database/InteractiveTableTupleTest.java
+++ src/test/database/InteractiveTableTupleTest.java
@@ -1,8 +1,11 @@
 package test.database;
 import static org.junit.Assert.*;
 
+import java.util.LinkedList;
+
 import org.junit.Test;
 import main.database.InteractiveTableTuple;
+import main.database.ReasonPair;
  
 /**
  * 
@@ -25,5 +28,22 @@ public class InteractiveTableTupleTest{
 		assertEquals(t.getName(), "Justin Bieber");
 		assertEquals((t.getRegularDate() + ""), "Fri Dec 02 00:00:00 IST 2016");
 		assertTrue(t.getKeyWords().isEmpty());
+	}		
+	@Test public void test2(){
+		InteractiveTableTuple t=new InteractiveTableTuple();
+		t.setName("Justin Bieber");
+		t.setDate( "12/02/2016");
+		assertEquals(t.getDate(), "12/02/2016");
+		assertEquals(t.getName(), "Justin Bieber");
+		assertEquals((t.getRegularDate() + ""), "Fri Dec 02 00:00:00 IST 2016");
+		assertTrue(t.getKeyWords().isEmpty());
+		ReasonPair rp=new ReasonPair();
+		rp.setProbability(0.4);
+		rp.setReason("stubbing");
+		LinkedList<ReasonPair> lst= new LinkedList<>();
+		lst.add(rp);
+		t.setReasons(lst);
+		assertEquals(t.getReasons().get(0).getProbability(), 0.4, 0);
+		assertEquals(t.getReasons().get(0).getReason(), "stubbing");
 	}		
 }
