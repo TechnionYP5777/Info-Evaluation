82ecdfade3d7b0587a51b6bbd2ea04430d169d7d
NetanelFelcher <netanel_felcher@hotmail.com>
2017-01-15 10:42:45 +0200

    added test for second c'tor #86

--- src/test/database/InteractiveTableTupleTest.java
+++ src/test/database/InteractiveTableTupleTest.java
@@ -1,6 +1,8 @@
 package test.database;
 import static org.junit.Assert.*;
 
+import java.util.Date;
+
 import org.junit.Test;
 import main.database.InteractiveTableTuple;
  
@@ -18,5 +20,12 @@ public class InteractiveTableTupleTest{
 		assertNull(t.getRegularDate());
 		assertTrue(t.getKeyWords().isEmpty());
 		assertTrue(t.getReasons().isEmpty());
+	}		
+	@Test public void test1(){
+		InteractiveTableTuple t=new InteractiveTableTuple("Justin Bieber", "12/02/2016", null);
+		assertEquals(t.getDate(), "12/02/2016");
+		assertEquals(t.getName(), "Justin Bieber");
+		assertEquals((t.getRegularDate() + ""), "Fri Dec 02 00:00:00 IST 2016");
+		assertTrue(t.getKeyWords().isEmpty());
 	}		
 }
