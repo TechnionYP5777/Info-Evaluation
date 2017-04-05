2e42709c6a5ca12de6f6c2c5d9d95ab0e04cde69
NetanelFelcher <netanel_felcher@hotmail.com>
2017-04-05 17:32:45 +0300

    Added Extractor class and extract persons from dbpedia #123

---
+++ InfoEvaluation/src/test/WikiDataTest/ExtractorTest.java
@@ -0,0 +1,14 @@
+package WikiDataTest;
+
+
+/**
+ * 
+ * @author Netanel
+ * @since 05-04-2017
+ * 
+ *
+ */
+public class ExtractorTest {
+
+	
+}
--- InfoEvaluation/src/test/WikiDataTest/WikiParsingTest.java
+++ InfoEvaluation/src/test/WikiDataTest/WikiParsingTest.java
@@ -9,7 +9,6 @@ import java.io.IOException;
  * 
  */
 import org.jsoup.*;
-import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.junit.Test;
@@ -18,21 +17,16 @@ public class WikiParsingTest {
 	
 	@Test
 	public void test1() throws IOException{
-		  String url = "http://en.wikipedia.org/wiki/New_York_City";
-		    Document doc = Jsoup.connect(url).get();
-		    Elements paragraphs = doc.select(".mw-content-ltr p, .mw-content-ltr li");
+		  Elements paragraphs = Jsoup.connect("http://en.wikipedia.org/wiki/New_York_City").get().select(".mw-content-ltr p, .mw-content-ltr li");
 
-		    Element firstParagraph = paragraphs.first();
-		    Element lastParagraph = paragraphs.last();
-		    Element p;
+		    Element firstParagraph = paragraphs.first(), lastParagraph = paragraphs.last(), p;
 		    int i = 1;
 		    p = firstParagraph;
 		    System.out.println(p.text());
-		    while (p != lastParagraph) {
-		        p = paragraphs.get(i);
-		        System.out.println(p.text());
-		        i++;
-		    }
+		    for (; p != lastParagraph; ++i) {
+				p = paragraphs.get(i);
+				System.out.println(p.text());
+			}
 		
 	}
 	
