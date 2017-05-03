7c93885dab3c9905fc71477183bbd0485b87b837
NetanelFelcher <netanel_felcher@hotmail.com>
2017-05-03 16:26:05 +0300

    deleted wrong code. the bug was on the config file, it is fixed now #136

--- src/infoeval/main/WikiData/Connector.java
+++ src/infoeval/main/WikiData/Connector.java
@@ -6,12 +6,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.text.SimpleDateFormat;
-import java.util.HashSet;
 import java.util.InvalidPropertiesFormatException;
-import java.util.List;
 import java.util.Properties;
-import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -38,10 +34,10 @@ public class Connector {
 		try {
 			conn = getConnection();
 			logger.log(Level.INFO, "connected to server");
-		} catch (Exception ¢) {
-			throw ¢;
+		} catch (Exception e) {
+			throw  e;
 		}
-		givePermissions();
+		
 		logger.log(Level.INFO, "database created succesfully");
 	}
 
@@ -49,8 +45,8 @@ public class Connector {
 		Properties props = new Properties();
 		try {
 			props.loadFromXML(getClass().getResourceAsStream("MySQLServerConfiguration.xml"));
-		} catch (InvalidPropertiesFormatException ¢) {
-			throw ¢;
+		} catch (InvalidPropertiesFormatException  e) {
+			throw  e;
 		}
 		username = props.getProperty("jdbc.username");
 		password = props.getProperty("jdbc.password");
@@ -61,25 +57,17 @@ public class Connector {
 				
 		try {
 			Class.forName(driver);
-		} catch (ClassNotFoundException ¢) {
-			throw ¢;
+		} catch (ClassNotFoundException e) {
+			throw e;
 		}
 		try {
 			return DriverManager.getConnection(server+"://"+host+"/"+db, username, password);
-		} catch (SQLException ¢) {
-			throw ¢;
+		} catch (SQLException e) {
+			throw e;
 		}
 	}
 
-	private void givePermissions() throws SQLException, IOException {
-		Properties props = new Properties();
-		try {
-			props.loadFromXML(getClass().getResourceAsStream("MySQLServerConfiguration.xml"));
-		} catch (InvalidPropertiesFormatException ¢) {
-			throw ¢;
-		}
-		runQuery("GRANT ALL PRIVILEGES ON "+ db +".* TO " + username + "@" + host + " IDENTIFIED BY " + password);
-	}
+	
 
 	public int runUpdate(final String query) throws SQLException {
 		return conn.createStatement().executeUpdate(query);
@@ -90,16 +78,10 @@ public class Connector {
 	}
 
 	public ResultSet runQuery(final String query, final Object[] inputs) throws SQLException {
-		PreparedStatement $ = conn.prepareStatement(query);
-		for (int ¢ = 1; ¢ <= inputs.length; ++¢)
-			$.setObject(¢, inputs[¢ - 1]);
-		return $.executeQuery();
-	}
-
-	public ResultSet runQuery(final String query, final Object input) throws SQLException {
-		PreparedStatement $ = conn.prepareStatement(query);
-		$.setObject(1, input);
-		return $.executeQuery();
+		PreparedStatement ps = conn.prepareStatement(query);
+		for (int i = 1;i <= inputs.length; ++i)
+			ps.setObject(i, inputs[1-i]);
+		return ps.executeQuery();
 	}
 
 	public int runUpdate(final String query, final Object input) throws SQLException {
@@ -112,8 +94,8 @@ public class Connector {
 		try {
 			if (conn != null)
 				conn.close();
-		} catch (final SQLException ¢) {
-			¢.printStackTrace();
+		} catch (final SQLException e) {
+			e.printStackTrace();
 		}
 	}
 }
