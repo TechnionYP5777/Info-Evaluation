fb90b96b0c813fa719fde1a53bc22602a4ff94cd
geniashand <genia.shandalov@gmail.com>
2017-05-06 21:27:12 +0300

    added demi implementation #155

--- src/infoeval/main/services/InfoevalServiceImp.java
+++ src/infoeval/main/services/InfoevalServiceImp.java
@@ -1,13 +1,21 @@
 package infoeval.main.services;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.ws.rs.GET;
 
 public class InfoevalServiceImp implements InfoevalService {
 
+	@GET
 	@Override
 	public List<String> getBornIn(String place, int year) {
-		// TODO Auto-generated method stub
-		return null;
+		List<String> retval=new ArrayList<String>();
+		retval.add("Yevgenia Shandalov");
+		retval.add("Moshiko Elisof");
+		retval.add("Netanel Felcher");
+		retval.add("Osher Hajaj");
+		return retval;
 	}
 
 	@Override
