157aa251a71d6fbfe507243d18b43b868380b7e1
geniashand <genia.shandalov@gmail.com>
2017-05-06 21:09:08 +0300

    created a class for web service use #155

---
+++ src/infoeval/main/services/InfoevalServiceImp.java
@@ -0,0 +1,19 @@
+package infoeval.main.services;
+
+import java.util.List;
+
+public class InfoevalServiceImp implements infoevalService {
+
+	@Override
+	public List<String> getBornIn(String place, int year) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<String> differentDeathPlace() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
