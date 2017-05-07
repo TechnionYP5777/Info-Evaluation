527a03e143fdc27cc62e2832c571c8fd57a7d297
MosheEliasof <moshikoe@campus.technion.ac.il>
2017-05-07 11:55:23 -0700

    Added a factory for pulling data from the web-service to the GUI , #155

--- GUI/www/js/services.js
+++ GUI/www/js/services.js
@@ -1,4 +1,7 @@
 /* global angular, document, window */
+
+// authors:Genia, Moshe
+// since : 6/5/2017
 'use strict';
 
 angular.module('starter.services', [])
@@ -21,6 +24,32 @@ angular.module('starter.services', [])
 		}
 	};
 })
+
+
+.factory('Query2Results',function($scope,$http,$ionicPopup,ApiEndpoint){
+	$scope.persons=[];
+    $http({
+      method: 'GET',
+      url: ApiEndpoint.url+ 'Query2',
+    }).then(function successCallback(response) {
+        $scope.persons = [];
+        for(var r in response.data) {
+          var person = response.data[r];
+          $scope.persons.push(person);
+        }
+
+    }, function errorCallback(response) {
+        var FetchErrorAlert = $ionicPopup.alert({
+            title: 'Fetch error!',
+            template: 'Unable to get data',
+        });
+    });
+
+	//End of this factory
+})
+
+
+//EOF
 ;
 
 
