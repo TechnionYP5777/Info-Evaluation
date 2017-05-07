8582b304bfc6cc0b37013d8712bd5ee25244331b
NetanelFelcher <netanel_felcher@hotmail.com>
2017-05-07 15:30:20 +0300

    last test #156

--- src/infoeval/test/mysqlTest/TableEntryTest.java
+++ src/infoeval/test/mysqlTest/TableEntryTest.java
@@ -51,4 +51,20 @@ public class TableEntryTest{
 		assertEquals("url", te.getUrl());
 		assertEquals("name", te.getName());
 	}
+	@Test
+	@SuppressWarnings("deprecation") public void test4(){
+		TableEntry te= new TableEntry("bla", "bla", "bla", "bla", new  Date(1,1,1), new Date(1,1,1));
+		te.setUrl("url");
+		te.setName("name");
+		te.setDeathPlace("deathPlace");
+		te.setBirthPlace("birthPlace");
+		te.setBirthDate(new Date(2000, 10, 21));
+		te.setDeathDate(new Date(2015, 10, 21));
+		assertEquals(te.getBirthDate(),new Date(2000, 10, 21) );
+		assertEquals(te.getDeathDate(),new Date(2015, 10, 21));
+		assertEquals("birthPlace", te.getBirthPlace());
+		assertEquals("deathPlace", te.getDeathPlace());
+		assertEquals("url", te.getUrl());
+		assertEquals("name", te.getName());
+	}
 }
