623eb7327f6dbd74d1c1bca2a94da5ee753e45fa
NetanelFelcher <netanel_felcher@hotmail.com>
2017-05-07 15:25:05 +0300

    second ctor test #156

--- src/infoeval/test/mysqlTest/TableEntryTest.java
+++ src/infoeval/test/mysqlTest/TableEntryTest.java
@@ -2,6 +2,8 @@ package infoeval.test.mysqlTest;
 
 import static org.junit.Assert.*;
 
+import java.sql.Date;
+
 import org.junit.Test;
 
 import infoeval.main.mysql.TableEntry;
@@ -22,5 +24,15 @@ public class TableEntryTest{
 		assertEquals("", te.getDeathPlace());
 		assertEquals("", te.getUrl());
 		assertEquals("", te.getName());
+	}
+	@Test
+	@SuppressWarnings("deprecation") public void test2(){
+		TableEntry te= new TableEntry("url", "name", "birthPlace", "deathPlace", new Date(2000, 10, 21), new Date(2015, 10, 21));
+		assertEquals(te.getBirthDate(),new Date(2000, 10, 21) );
+		assertEquals(te.getDeathDate(),new Date(2015, 10, 21));
+		assertEquals("birthPlace", te.getBirthPlace());
+		assertEquals("deathPlace", te.getDeathPlace());
+		assertEquals("url", te.getUrl());
+		assertEquals("name", te.getName());
 	}
 }
