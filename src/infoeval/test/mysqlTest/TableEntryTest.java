01f8f9e449c44d2831dcde281188e048ce4c8e00
NetanelFelcher <netanel_felcher@hotmail.com>
2017-05-07 15:28:01 +0300

    setters test #156

--- src/infoeval/test/mysqlTest/TableEntryTest.java
+++ src/infoeval/test/mysqlTest/TableEntryTest.java
@@ -35,4 +35,20 @@ public class TableEntryTest{
 		assertEquals("url", te.getUrl());
 		assertEquals("name", te.getName());
 	}
+	@Test
+	@SuppressWarnings("deprecation") public void test3(){
+		TableEntry te= new TableEntry();
+		te.setUrl("url");
+		te.setName("name");
+		te.setDeathPlace("deathPlace");
+		te.setBirthPlace("birthPlace");
+		te.setBirthDate(new Date(2000, 10, 21));
+		te.setDeathDate(new Date(2015, 10, 21));
+		assertEquals(te.getBirthDate(),new Date(2000, 10, 21) );
+		assertEquals(te.getDeathDate(),new Date(2015, 10, 21));
+		assertEquals("birthPlace", te.getBirthPlace());
+		assertEquals("deathPlace", te.getDeathPlace());
+		assertEquals("url", te.getUrl());
+		assertEquals("name", te.getName());
+	}
 }
