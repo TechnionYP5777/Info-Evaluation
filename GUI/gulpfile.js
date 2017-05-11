293bdca11d472345ce4c83d0b5717f3c81bd4296
MosheEliasof <moshikoalis@gmail.com>
2017-05-11 11:24:01 +0300

    fix CORS

--- GUI/gulpfile.js
+++ GUI/gulpfile.js
@@ -17,7 +17,7 @@ var replaceFiles = ['./www/js/controllers.js'];
 
 gulp.task('add-proxy', function() {
   return replace({
-    regex: "http://cors.api.com/api",
+    regex: "http://localhost:8080/Queries/Query2",
     replacement: "http://localhost:8100/api",
     paths: replaceFiles,
     recursive: false,
@@ -28,7 +28,7 @@ gulp.task('add-proxy', function() {
 gulp.task('remove-proxy', function() {
   return replace({
     regex: "http://localhost:8100/api",
-    replacement: "http://cors.api.com/api",
+    replacement: "http://localhost:8080/Queries/Query2",
     paths: replaceFiles,
     recursive: false,
     silent: false,
