c4c227266d04c6831e8467e59ceaf864825be9e9
osherh <osherh@campus.technion.ac.il>
2017-05-16 10:16:32 -0700

    #159: added sql queries that return table entries

--- src/infoeval/main/mysql/SqlQueriesRunner.java
+++ src/infoeval/main/mysql/SqlQueriesRunner.java
@@ -1,8 +1,10 @@
 package infoeval.main.mysql;
 
+import java.sql.Date;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 import infoeval.main.WikiData.Connector;
@@ -12,43 +14,53 @@ import infoeval.main.WikiData.Connector;
  * @author osherh
  * @Since 12-05-2017
  * 
- * This class runs SQL queries on mysql server and returns a list of table entry as results
+ *        This class runs SQL queries on mysql server and returns a list of
+ *        table entry as results
  *
  */
 public class SqlQueriesRunner {
-	private String query1;
-	private String query2; 
 	private Connector conn;
+	private String wikiURL = "https://en.wikipedia.org/?curid=";
 
-	public SqlQueriesRunner() throws Exception{
-		query1 = "SELECT * "
-				+ "FROM basic_info "
-				+ "WHERE YEAR(BirthDate) < ? "
-				+ "AND BirthPlace = ? ";
-		query2 = "SELECT * "
-				+ "FROM basic_info "
-				+ "WHERE BirthPlace != DeathPlace ";
+	public SqlQueriesRunner() throws Exception {
 		conn = new Connector();
 	}
-	//params has year and then place
-	public List<TableEntry> runQuery(int queryNum, Object[] params) throws SQLException{
-		if(queryNum == 1){
-			ResultSet rs = conn.runQuery(query1,params);
-			ArrayList<TableEntry> $ = new ArrayList<TableEntry>();
-			while(rs.next()){
-				TableEntry te = new TableEntry("",rs.getString(1),rs.getString(2),rs.getString(3),rs.getDate(4),rs.getDate(5));
-				$.add(te);
-			}
-			return $;
-		}	
-		if (queryNum != 2)
-			return null;
-		ResultSet rs = conn.runQuery(query2);
+
+	public List<TableEntry> getBornInPlaceBeforeYear(String place, int year) throws SQLException {
+		final String yearPlaceQuery = "SELECT basic_info.name,BirthDate,wikiPageID FROM basic_info,WikiID "
+				+ "WHERE YEAR(BirthDate) < ? AND BirthPlace = ? "
+				+ "AND wikiPageID = (SELECT wikiPageID FROM WikiID WHERE WikiID.name = basic_info.name LIMIT 1)";
+		Object[] inp = new Object[] { place, year };
+		ResultSet rs = conn.runQuery(yearPlaceQuery, inp);
 		ArrayList<TableEntry> res = new ArrayList<TableEntry>();
 		while (rs.next()) {
-			TableEntry te = new TableEntry("", rs.getString(1), rs.getString(2), rs.getString(3), rs.getDate(4),
-					rs.getDate(5));
-			res.add(te);
+			String name = rs.getString(1);
+			Date birthDate = rs.getDate(2);
+			String wikiPageID = rs.getString(3);
+			Calendar cal1 = Calendar.getInstance();
+			cal1.set(Calendar.YEAR, 1912);
+			cal1.set(Calendar.MONTH, Calendar.JUNE);
+			cal1.set(Calendar.DAY_OF_MONTH, 23);
+			res.add(new TableEntry(wikiURL + wikiPageID, name, "", "", birthDate, (Date) cal1.getTime()));
+		}
+		return res;
+	}
+
+	public List<TableEntry> getDifferentDeathPlace() throws SQLException {
+		final String birthDeathPlaceQuery = "SELECT basic_info.name,BirthPlace,DeathPlace,wikiPageID "
+				+ "FROM basic_info,WikiID WHERE DeathPlace IS NOT NULL AND BirthPlace != DeathPlace "
+				+ "AND wikiPageID = (SELECT wikiPageID FROM WikiID WHERE WikiID.name = basic_info.name LIMIT 1)";
+		ResultSet rs = conn.runQuery(birthDeathPlaceQuery);
+		ArrayList<TableEntry> res = new ArrayList<TableEntry>();
+		while (rs.next()) {
+			String name = rs.getString(1), birthPlace = rs.getString(2), deathPlace = rs.getString(3),
+					wikiPageID = rs.getString(4);
+			Calendar cal1 = Calendar.getInstance();
+			cal1.set(Calendar.YEAR, 1912);
+			cal1.set(Calendar.MONTH, Calendar.JUNE);
+			cal1.set(Calendar.DAY_OF_MONTH, 23);
+			Date dummyDate = (Date) cal1.getTime();
+			res.add(new TableEntry(wikiURL + wikiPageID, name, birthPlace, deathPlace, dummyDate, dummyDate));
 		}
 		return res;
 	}
--- src/infoeval/test/mysqlTest/SqlQueriesRunnerTest.java
+++ src/infoeval/test/mysqlTest/SqlQueriesRunnerTest.java
@@ -9,7 +9,6 @@ import static org.junit.Assert.*;
 
 import java.text.SimpleDateFormat;
 
-
 /**
  * 
  * @author osherh
@@ -18,21 +17,21 @@ import java.text.SimpleDateFormat;
  */
 public class SqlQueriesRunnerTest {
 	@Test
-	public void query1Test() throws Exception{
-		for (TableEntry te : (new SqlQueriesRunner()).runQuery(1, (new Object[] { 1993, "London" }))) {
+	public void getBornInPlaceBeforeYearTest() throws Exception {
+		for (TableEntry te : (new SqlQueriesRunner()).getBornInPlaceBeforeYear("London", 1993)) {
 			java.sql.Date birthDate = te.getBirthDate();
 			String birthPlace = te.getBirthPlace();
 			SimpleDateFormat formatYear = new SimpleDateFormat("yyyy");
 			String birthYear = formatYear.format(birthDate);
 			int bYear = Integer.parseInt(birthYear);
-			assertTrue(bYear < 1993);
+			assert bYear < 1993;
 			assertEquals(birthPlace, "London");
 		}
 	}
-	
+
 	@Test
-	public void query2Test() throws Exception{
-		for (TableEntry ¢ : (new SqlQueriesRunner()).runQuery(2, null))
+	public void getDifferentDeathPlaceTest() throws Exception {
+		for (TableEntry ¢ : (new SqlQueriesRunner()).getDifferentDeathPlace())
 			assertNotEquals(¢.getBirthPlace(), ¢.getDeathPlace());
 	}
 }
