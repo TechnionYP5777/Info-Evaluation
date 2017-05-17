78465b1c45c7c0218bd1612d04ae2c42a588755e
NetanelFelcher <netanel_felcher@hotmail.com>
2017-05-17 16:52:58 +0300

    rearrange query string to make it clearer#172

--- src/infoeval/main/WikiData/Extractor.java
+++ src/infoeval/main/WikiData/Extractor.java
@@ -26,19 +26,36 @@ public class Extractor {
 	private static final int SKIP_NUM = 10000;	
 	
 	public Extractor() {
-		basicInfoQuery = new ParameterizedSparqlString("PREFIX  rdf:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#>"
-				+ " PREFIX  dbo: <http://dbpedia.org/ontology/> PREFIX  dbp: <http://dbpedia.org/property/>"
-				+ " SELECT DISTINCT  ?name (SAMPLE (?occupation) as ?occup)(SAMPLE (?spouse) as ?spouses)(SAMPLE (?spName) as ?sname)(SAMPLE (?spOccupation) as ?spOccu)(SAMPLE (?deathPlace) as ?death)  (SAMPLE(?birthPlace) as ?birth) (SAMPLE(?deathDate) as ?dDate) (SAMPLE( ?birthDate) as ?bDate) "
+		basicInfoQuery = new ParameterizedSparqlString
+				("  PREFIX  rdf:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#>"
+				+ " PREFIX  dbo: <http://dbpedia.org/ontology/>"
+				+ " PREFIX  dbp: <http://dbpedia.org/property/>"
+				+ " SELECT DISTINCT "
+					+ " ?name (SAMPLE (?occupation) as ?occup)"
+					+ "(SAMPLE (?spouse) as ?spouses) (SAMPLE (?spName) as ?sname)"
+					+ "(SAMPLE (?spOccupation) as ?spOccu) (SAMPLE (?deathPlace) as ?death)"
+					+ "(SAMPLE(?birthPlace) as ?birth) (SAMPLE(?deathDate) as ?dDate) "
+					+ "(SAMPLE( ?birthDate) as ?bDate) "
 				+ " WHERE {"
-				+ "  ?resource   a <http://dbpedia.org/ontology/Person>;    dbp:name ?name; dbp:birthPlace ?birthPlace;dbp:birthDate ?birthDate.OPTIONAL{?resource dbo:occupation ?occupation}.OPTIONAL{?resource dbo:spouse ?spouse. ?spouse dbp:name ?spName. ?spouse dbo:occupation ?spOccupation}. OPTIONAL{?resource dbp:deathDate ?deathDate. ?resource dbp:deathPlace ?deathPlace} "
+					+ " ?resource   a <http://dbpedia.org/ontology/Person>;  "
+					+ " dbp:name ?name; dbp:birthPlace ?birthPlace;dbp:birthDate ?birthDate."
+					+ " OPTIONAL{?resource dbo:occupation ?occupation}."
+					+ " OPTIONAL{?resource dbo:spouse ?spouse. ?spouse dbp:name ?spName."
+							+ " ?spouse dbo:occupation ?spOccupation}. "
+					+ " OPTIONAL{?resource dbp:deathDate ?deathDate. ?resource dbp:deathPlace ?deathPlace} "
 				+ " FILTER (lang(?name) = 'en')  }GROUP BY ?name "
 				+ " ORDER BY DESC(?name)    LIMIT "+ENTRIES_NUM+" OFFSET "+ENTRIES_NUM);
 
 		wikiIdQuery = new ParameterizedSparqlString(
-				"PREFIX  rdf:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#> PREFIX  dbo: <http://dbpedia.org/ontology/>"
-						+ " PREFIX  dbp: <http://dbpedia.org/property/> SELECT DISTINCT ?name ?wikiPageID WHERE { "
-						+ " ?resource a <http://dbpedia.org/ontology/Person>; dbp:name ?name; dbo:wikiPageID ?wikiPageID."
-						+ " FILTER (lang(?name) = 'en')} ORDER BY DESC(?name) LIMIT "+ENTRIES_NUM+" OFFSET "+SKIP_NUM);
+					"PREFIX  rdf:  <http://www.w3.org/1999/02/22-rdf-syntax-ns#> "
+			   	+   "PREFIX  dbo: <http://dbpedia.org/ontology/>"
+				+   "PREFIX  dbp: <http://dbpedia.org/property/> "
+				+   "SELECT DISTINCT ?name ?wikiPageID "
+				+   "WHERE { "
+						+ " ?resource a <http://dbpedia.org/ontology/Person>; dbp:name ?name;"
+						+ "  dbo:wikiPageID ?wikiPageID."
+						+ " FILTER (lang(?name) = 'en')} "
+						+ "ORDER BY DESC(?name) LIMIT "+ENTRIES_NUM+" OFFSET "+SKIP_NUM);
 
 		queriesMap = new HashMap<QueryTypes, ParameterizedSparqlString>();
 		queriesMap.put(QueryTypes.BASIC_INFO, basicInfoQuery);
