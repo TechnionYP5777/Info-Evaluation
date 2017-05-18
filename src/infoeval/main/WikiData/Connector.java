fb0191f179c3078db3fc80859cfca609c486b2c7
osherh <osherh@campus.technion.ac.il>
2017-05-17 22:26:07 -0700

    #164 conector works

--- src/infoeval/main/WikiData/Connector.java
+++ src/infoeval/main/WikiData/Connector.java
@@ -6,6 +6,7 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.InvalidPropertiesFormatException;
@@ -33,21 +34,17 @@ public class Connector {
 	private String host;
 	private String db;
 	private Connection conn;
-	private String[] months = { "January", "February", "March", "April", "May", "June", "July", "August", "September",
-			"October", "November", "December" };
-
+	
 	public Connector() throws Exception {
 		try {
 			conn = getConnection();
 			logger.log(Level.INFO, "connected to server");
-			createTables();
-			fillBasicInfoTable();
-			fillWikiIdTable();
+			//createTables();
 		} catch (Exception e) {
 			throw e;
 		}
 	}
-
+	
 	public Connection getConnection() throws SQLException, IOException, ClassNotFoundException {
 		Properties props = new Properties();
 		try {
@@ -74,159 +71,61 @@ public class Connector {
 		}
 	}
 
-	public void createTables() throws SQLException {
-		runUpdate("CREATE TABLE IF NOT EXISTS basic_info(Name VARCHAR(100) NOT NULL,"
-				+ " BirthPlace VARCHAR(100) NULL,DeathPlace VARCHAR(100) NULL,BirthDate DATE NULL,"
-				+ " DeathDate DATE NULL)");
-		logger.log(Level.INFO, "basic_info table created successfully");
-		runUpdate("CREATE TABLE IF NOT EXISTS WikiID(Name VARCHAR(100) NOT NULL,wikiPageID VARCHAR(50) NOT NULL)");
-		logger.log(Level.INFO, "WIKI_ID table created successfully");
-	}
-
-	public void fillBasicInfoTable() throws SQLException, ParseException {
-		clearBasicInfoTable();
-		Extractor ext = new Extractor();
-		ext.executeQuery(QueryTypes.BASIC_INFO);
-		ResultSetRewindable results = ext.getResults();
-		results.reset();
-		for (int i = 0; i < results.size(); ++i) {
-
-			QuerySolution solution = results.nextSolution();
-
-			String name = solution.getLiteral("name").getString();
-
-			RDFNode bPlace = solution.get("birth");
-			String birthPlace = null;
-			if (bPlace != null)
-				if (bPlace.isResource())
-					birthPlace = (bPlace.asResource() + "").split("resource/")[1];
-				else if (bPlace.isLiteral())
-					birthPlace = (bPlace.asLiteral() + "").split("@")[0];
-
-			RDFNode dPlace = solution.get("death");
-			String deathPlace = null;
-			if (dPlace != null)
-				if (dPlace.isResource())
-					deathPlace = (dPlace.asResource() + "").split("resource/")[1];
-				else if (dPlace.isLiteral())
-					deathPlace = (dPlace.asLiteral() + "").split("@")[0];
-
-			RDFNode bDate = solution.get("bDate");
-			String birthDate = !bDate.isLiteral() ? null : bDate.asLiteral().getValue() + "";
-
-			RDFNode dDate = solution.get("dDate");
-			String deathDate = null;
-			java.sql.Date sqlDeathDate = null;
-			if (dDate == null)
-				sqlDeathDate = null;
-			else {
-				deathDate = !dDate.isLiteral() ? null : dDate.asLiteral().getValue() + "";
-				if (deathDate.contains("."))
-					sqlDeathDate = null;
-				else if (deathDate.split("-").length == 1 && deathDate.matches("[0-9]+") && deathDate.length() <= 4)
-					sqlDeathDate = stringToSqlDate(deathDate, new SimpleDateFormat("yyyy"));
-				else if (deathDate.matches("[0-9][0-9][0-9][0-9][-][0-9][0-9][-][0-9][0-9]"))
-					sqlDeathDate = stringToSqlDate(deathDate, new SimpleDateFormat("yyyy-MM-dd"));
-				int monthNum = 1;
-				for (String month : months) {
-					if (deathDate.equals(month)) {
-						sqlDeathDate = stringToSqlDate(monthNum + "", new SimpleDateFormat("MM"));
-						break;
-					}
-					if (deathDate.startsWith(month)) {
-						String parseDeathDate = deathDate.split(" ")[1] + "-" + monthNum;
-						sqlDeathDate = stringToSqlDate(parseDeathDate, new SimpleDateFormat("yyyy-MM"));
-						break;
-					}
-					++monthNum;
-				}
-
-			}
-
-			java.sql.Date sqlBirthDate = null;
-			if (birthDate.contains("."))
-				sqlBirthDate = null;
-			else if (birthDate.split("-").length == 1 && birthDate.matches("[0-9]+") && birthDate.length() <= 4)
-				sqlBirthDate = stringToSqlDate(birthDate, new SimpleDateFormat("yyyy"));
-			else if (birthDate.startsWith("--") && birthDate.split("--").length == 2)
-				sqlBirthDate = stringToSqlDate(birthDate, new SimpleDateFormat("MM-dd"));
-			else if (birthDate.matches("[0-9][0-9][0-9][0-9][-][0-9][0-9][-][0-9][0-9]"))
-				sqlBirthDate = stringToSqlDate(birthDate, new SimpleDateFormat("yyyy-MM-dd"));
-			int monthNum = 1;
-			for (String month : months) {
-				if (birthDate.equals(month)) {
-					sqlBirthDate = stringToSqlDate(monthNum + "", new SimpleDateFormat("MM"));
-					break;
-				}
-				if (birthDate.startsWith(month)) {
-					String parseBirthDate = birthDate.split(" ")[1] + "-" + monthNum;
-					sqlBirthDate = stringToSqlDate(parseBirthDate, new SimpleDateFormat("yyyy-MM"));
-					break;
-				}
-				++monthNum;
-			}
-
-			Object[] inp = new Object[5];
-			inp[0] = name;
-			inp[1] = birthPlace;
-			inp[2] = deathPlace;
-			inp[3] = sqlBirthDate;
-			inp[4] = sqlDeathDate;
-			runUpdate("INSERT INTO basic_info VALUES(?,?,?,?,?)", inp);
-		}
-	}
-
-	public void fillWikiIdTable() throws SQLException {
-		clearWikiIdTable();
-		Extractor ext = new Extractor();
-		ext.executeQuery(QueryTypes.WIKI_ID);
-		ResultSetRewindable results = ext.getResults();
-		results.reset();
-		for (int i = 0; i < results.size(); ++i) {
-			QuerySolution solution = results.nextSolution();
-			Object[] inp = new Object[2];
-			inp[0] = solution.getLiteral("name").getString();
-			inp[1] = solution.getLiteral("wikiPageID").getString();
-			runUpdate("INSERT INTO WikiID VALUES(?,?)", inp);
-		}
-	}
-
-	private java.sql.Date stringToSqlDate(String stringDate, SimpleDateFormat f) throws ParseException {
-		return new java.sql.Date(f.parse(stringDate).getTime());
-	}
-
 	public int runUpdate(final String query) throws SQLException {
-		return conn.createStatement().executeUpdate(query);
+		//conn = getConnection();
+		//logger.log(Level.INFO, "connected to server in run update");		
+		int res = conn.createStatement().executeUpdate(query);
+		//closeConnection();
+		//logger.log(Level.INFO, "close connection in run update");
+		return res;
 	}
 
-	public ResultSet runQuery(final String query) throws SQLException {
-		return conn.createStatement().executeQuery(query);
+	public ResultSet runQuery(final String query) throws SQLException{
+		//conn = getConnection();
+		//logger.log(Level.INFO, "connected to server in run query");		
+		Statement s = conn.createStatement();
+		ResultSet res = s.executeQuery(query);
+		//closeConnection();
+		//logger.log(Level.INFO, "close connection in run query");
+		return res;
 	}
 
 	public ResultSet runQuery(final String query, final Object[] inputs) throws SQLException {
+		//conn = getConnection();
+		//logger.log(Level.INFO, "get connection in run query with params");		
 		PreparedStatement ps = conn.prepareStatement(query);
 		for (int ¢ = 1; ¢ <= inputs.length; ++¢)
 			ps.setObject(¢, inputs[¢ - 1]);
+		//closeConnection();
+		//logger.log(Level.INFO, "close connection in run query with params");
 		return ps.executeQuery();
 	}
 
 	public int runUpdate(final String query, final Object[] inputs) throws SQLException {
+		//conn = getConnection();		
+		//logger.log(Level.INFO, "get connection in run update array");
 		PreparedStatement ps = conn.prepareStatement(query);
 		for (int ¢ = 1; ¢ <= inputs.length; ++¢)
 			ps.setObject(¢, inputs[¢ - 1]);
+		//closeConnection();
+		//logger.log(Level.INFO, "close connection in run update array");
 		return ps.executeUpdate();
 	}
 
 	public int runUpdate(final String query, final Object input) throws SQLException {
+		//conn = getConnection();		
+		//logger.log(Level.INFO, "get connection in run update");
 		PreparedStatement $ = conn.prepareStatement(query);
 		$.setObject(1, input);
+		//closeConnection();
+		//logger.log(Level.INFO, "close connection in run update");
 		return $.executeUpdate();
 	}
 
-	public void dropTables() throws SQLException {
-		runUpdate("DROP TABLE basic_info");
-		runUpdate("DROP TABLE WikiID");
-	}
+	//public void dropTables() throws SQLException {
+	//	runUpdate("DROP TABLE basic_info");
+	//	runUpdate("DROP TABLE WikiID");
+	//}
 
 	public void clearBasicInfoTable() throws SQLException {
 		runUpdate("DELETE FROM basic_info");
