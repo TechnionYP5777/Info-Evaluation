ee2494a851b5adaf609151e42de0fb9ba3859b4e
MosheEliasof <moshikoe@campus.technion.ac.il>
2017-05-19 12:57:22 -0700

    Server is back again. Also, fixed the test file . #173

---
+++ src/infoeval/test/mysqlTest/sqlRunnerTest.java
@@ -0,0 +1,46 @@
+package infoeval.test.mysqlTest;
+
+
+
+import org.junit.Test;
+
+import infoeval.main.mysql.SqlQueriesRunner;
+import infoeval.main.mysql.TableEntry;
+
+import static org.junit.Assert.*;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 
+ * @author Moshe
+ * @Since 12-05-2017
+ *
+ */
+public class sqlRunnerTest {
+	/**
+	 * [[SuppressWarningsSpartan]]
+	 */
+	@Test
+	public void getBornInPlaceBeforeYearTest() throws Exception {
+		List<TableEntry> lst = (new SqlQueriesRunner()).getBornInPlaceBeforeYear("London", "1993");
+		for (TableEntry te : lst) {
+			java.sql.Date birthDate = te.getBirthDate();
+			String birthPlace = te.getBirthPlace();
+			SimpleDateFormat formatYear = new SimpleDateFormat("yyyy");
+			String birthYear = formatYear.format(birthDate);
+			int bYear = Integer.parseInt(birthYear);
+			assert bYear < 1993;
+			assertEquals(birthPlace, "London");
+		}
+	}
+
+//	@Test
+//	public void getDifferentDeathPlaceTest() throws Exception {
+//		for (TableEntry ¢ : (new SqlQueriesRunner()).getDifferentDeathPlace())
+//			assertNotEquals(¢.getBirthPlace(), ¢.getDeathPlace());
+//	}
+}
+
