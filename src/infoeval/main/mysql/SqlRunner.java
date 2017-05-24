b9db3dc71097a450561b44c67d4d9acb790e85a6
osherh <osherh@campus.technion.ac.il>
2017-05-24 02:19:45 -0700

    #161 #177 add new queries, and start to add select queries caching

--- src/infoeval/main/mysql/SqlRunner.java
+++ src/infoeval/main/mysql/SqlRunner.java
@@ -5,14 +5,11 @@ import java.sql.Date;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Map.Entry;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-
-
-
 import infoeval.main.WikiData.Connector;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /** 
  * @author osherh , Moshe
@@ -24,28 +21,29 @@ public class SqlRunner {
 	private Connector conn;
 	private String wikiURL = "https://en.wikipedia.org/?curid=";
 	private static final Logger logger = Logger.getLogger("SqlRunner".getClass().getName());
+	
 	public SqlRunner() throws Exception {
 		conn = new Connector();
 	}
 	
 	public void close(){
 		try{
-		conn.close();
+			conn.close();
 		}
 		catch(SQLException e){
 			e.printStackTrace();
 		}
 	}
-	
+		
 	//@RequestMapping("Queries/Query2_real")
 	public ArrayList<TableEntry> getBornInPlaceBeforeYear(String place, String year)
 			throws SQLException, ClassNotFoundException, IOException,ParseException {
-		//int yearVal = Integer.parseInt(year);
-		final String yearPlaceQuery = "SELECT basic_info.name,BirthDate,wikiPageID FROM basic_info,WikiID "
+		logger.log(Level.INFO, "Born and died in different place is being executed");
+		final String beforeYearInPlace = "SELECT SQL_CACHE basic_info.name,BirthDate,wikiPageID FROM basic_info,WikiID "
 				+ "WHERE YEAR(BirthDate) < ? AND BirthPlace = ? "
 				+ "AND wikiPageID = (SELECT wikiPageID FROM WikiID WHERE WikiID.name = basic_info.name LIMIT 1)";
 		Object[] inp = new Object[] { year, place };
-		ArrayList<Row> rs = conn.runQuery(yearPlaceQuery, inp);
+		ArrayList<Row> rs = conn.runQuery(beforeYearInPlace, inp);
 		ArrayList<TableEntry> res = new ArrayList<TableEntry>();
 		for (Row row : rs){
 			String name = (String) row.row.get(0).getValue().cast(row.row.get(0).getKey()); 
@@ -53,29 +51,23 @@ public class SqlRunner {
 			String wikiPageID = (String) row.row.get(2).getValue().cast(row.row.get(2).getKey());
 			SimpleDateFormat df = new SimpleDateFormat("MM-dd-yyyy");
 			java.sql.Date sqlDate = new java.sql.Date(df.parse("1970-12-07").getTime());
-			res.add(new TableEntry(wikiURL + wikiPageID, name,place,"", birthDate,sqlDate ,"","",""));
+			res.add(new TableEntry(wikiURL + wikiPageID, name, place, "", birthDate, sqlDate,"","",""));
 		 }
-		//TODO: delete
-		//printing
-		for (Row row : rs){
-		    for (Entry<Object, Class> col: row.row){
-		        System.out.print(" > " + ((col.getValue()).cast(col.getKey())));
-		    }
-		    System.out.println();
-		}
 		return res;
 	}
 
 	public ArrayList<TableEntry> getDifferentDeathPlace() throws SQLException, ClassNotFoundException, IOException, ParseException {
-		logger.log(Level.INFO, "Born and died in different place is being executed");
-		final String birthDeathPlaceQuery = "SELECT basic_info.name,BirthPlace,DeathPlace,wikiPageID "
-				+ "FROM basic_info,WikiID WHERE DeathPlace IS NOT NULL AND BirthPlace != DeathPlace "
+		logger.log(Level.INFO, "Different birth and death place is being executed");
+		final String birthDeathPlace = "SELECT SQL_CACHE basic_info.name,BirthPlace,DeathPlace,wikiPageID "
+				+ "FROM basic_info,WikiID "
+				+ "WHERE DeathPlace != 'No Death Place' "
+				+ "AND BirthPlace != DeathPlace "
 				+ "AND wikiPageID = (SELECT wikiPageID FROM WikiID WHERE WikiID.name = basic_info.name LIMIT 1)";
-		ArrayList<Row> rs = conn.runQuery(birthDeathPlaceQuery);
+		ArrayList<Row> rs = conn.runQuery(birthDeathPlace);
 		ArrayList<TableEntry> res = new ArrayList<TableEntry>();
+		int i=1;
 		for (Row row : rs){
-			logger.log(Level.INFO, "One more added.");
-
+			logger.log(Level.INFO, "record num " + i + " added");
 			String name = (String) row.row.get(0).getValue().cast(row.row.get(0).getKey()); 
 			String birthPlace = (String) row.row.get(1).getValue().cast(row.row.get(1).getKey()); 
 			String deathPlace = (String) row.row.get(2).getValue().cast(row.row.get(2).getKey());
@@ -83,32 +75,94 @@ public class SqlRunner {
 			SimpleDateFormat df = new SimpleDateFormat("MM-dd-yyyy");
 			java.sql.Date sqlDate = new java.sql.Date(df.parse("1970-12-07").getTime());
 			res.add(new TableEntry(wikiURL + wikiPageID, name, birthPlace, deathPlace,sqlDate ,sqlDate ,"","",""));
-		 }
-		logger.log(Level.INFO, "Finished");
-		return res;
-	}
-	
-	//TODO: I want to return their wiki pages ID's too. Got to design a solution for that 
-	public ArrayList<TableEntry> getSameOccupationCouples() throws SQLException, ClassNotFoundException, IOException, ParseException {
-		//TODO: fix query
-		final String sameOccupationCouplesQuery = "SELECT B1.name AS Name,B2.name AS SpouseName,B1.occupation AS Occpation "
-				+ "FROM basic_info B1, basic_info B2 "
-				+ "WHERE B1.spouseName = B2.name AND B2.spouseName = B1.name "
-				+ "AND B1.occupation = B2.occupation"; 
-		ArrayList<Row> rs = conn.runQuery(sameOccupationCouplesQuery);
-		ArrayList<TableEntry> res = new ArrayList<TableEntry>();
-		for (Row row : rs){
-			String name = (String) row.row.get(0).getValue().cast(row.row.get(0).getKey()); 
-			//String nameWikiID = (String) row.row.get(1).getValue().cast(row.row.get(1).getKey()); 
-			String spouseName = (String) row.row.get(1).getValue().cast(row.row.get(1).getKey()); 
-			//String spouseNameWikiID = (String) row.row.get(3).getValue().cast(row.row.get(3).getKey()); 		
-			String occupation = (String) row.row.get(2).getValue().cast(row.row.get(2).getKey());
-			SimpleDateFormat df = new SimpleDateFormat("MM-dd-yyyy");
-			java.sql.Date sqlDate = new java.sql.Date(df.parse("1970-12-07").getTime());
-			res.add(new TableEntry("", name, "", "",sqlDate ,sqlDate ,"","",occupation));
+			++i;
 		}
 		return res;
 	}
 	
+	public ArrayList<TableEntry> getSameOccupationCouples() throws SQLException, ClassNotFoundException, IOException, ParseException {
+		logger.log(Level.INFO, "Same occupation couples is being executed");
+		final String sameOccupationCouples = "SELECT SQL_CACHE name,spouseName,occupation,spouseOccupation "
+				+ "FROM basic_info "
+				+ "WHERE spouseName != 'No Spouse Name' AND spouseOccupation != 'No Spouse Occupation' "
+				+ "AND occupation = spouseOccupation"; 		
+		ArrayList<Row> rs = conn.runQuery(sameOccupationCouples); 
+		ArrayList<TableEntry> res = new ArrayList<TableEntry>();
+		for (Row row : rs){
+			String name = (String) row.row.get(0).getValue().cast(row.row.get(0).getKey()); 
+			String spouseName = (String) row.row.get(1).getValue().cast(row.row.get(1).getKey()); 
+			String occupation = (String) row.row.get(2).getValue().cast(row.row.get(2).getKey());
+			String spouseOoccupation = (String) row.row.get(2).getValue().cast(row.row.get(2).getKey());
+			SimpleDateFormat df = new SimpleDateFormat("MM-dd-yyyy");
+			java.sql.Date sqlDate = new java.sql.Date(df.parse("1970-12-07").getTime());
+			res.add(new TableEntry("", name, "", "",sqlDate ,sqlDate ,occupation, spouseName,spouseOoccupation));
+		}
+		return res;
+	}
+	
+	public ArrayList<TableEntry> getSameBirthPlaceCouples() throws SQLException, ClassNotFoundException, IOException, ParseException {
+		logger.log(Level.INFO, "same birth place couples is being executed");
+		final String sameBirthPlaceCouples = "SELECT SQL_CACHE B1.name AS Name,B2.name AS SpouseName,B1.birthPlace AS BirthPlace "
+				+ "FROM basic_info B1, basic_info B2 "
+				+ "WHERE B1.spouseName = B2.name AND B2.spouseName = B1.name "
+				+ "AND B1.birthPlace = B2.birthPlace"; 
+		ArrayList<Row> rs = conn.runQuery(sameBirthPlaceCouples); 
+		ArrayList<TableEntry> res = new ArrayList<TableEntry>();
+		for (Row row : rs){
+			String name = (String) row.row.get(0).getValue().cast(row.row.get(0).getKey()); 
+			String spouseName = (String) row.row.get(1).getValue().cast(row.row.get(1).getKey()); 
+			String birthPlace = (String) row.row.get(2).getValue().cast(row.row.get(2).getKey());
+			SimpleDateFormat df = new SimpleDateFormat("MM-dd-yyyy");
+			java.sql.Date sqlDate = new java.sql.Date(df.parse("1970-12-07").getTime());
+			res.add(new TableEntry("", name, birthPlace, "", sqlDate, sqlDate, "", spouseName, ""));
+		}
+		return res;
+	}
 
+	public ArrayList<TableEntry> getOccupationBetweenYears(String year1, String year2, String occupation) throws SQLException, ClassNotFoundException, IOException, ParseException {
+		logger.log(Level.INFO, "occupation between years is being executed");
+		final String occupationBetweenYears = "SELECT SQL_CACHE name,birthDate,deathDate,wikiPageId "
+				+ "FROM basic_info, WikiID "
+				+ "WHERE deathDate IS NOT NULL "
+				+ "AND YEAR(birthDate) >= ? AND YEAR(deathDate) <= ? "
+				+ "AND wikiPageID = (SELECT wikiPageID FROM WikiID WHERE WikiID.name = basic_info.name LIMIT 1)"; 
+		Object[] inp = new Object[] { year1, year2, occupation };
+		ArrayList<Row> rs = conn.runQuery(occupationBetweenYears,inp); 
+		ArrayList<TableEntry> res = new ArrayList<TableEntry>();
+		for (Row row : rs){
+			String name = (String) row.row.get(0).getValue().cast(row.row.get(0).getKey()); 
+			Date birthDate = (java.sql.Date) row.row.get(1).getValue().cast(row.row.get(1).getKey()); 
+			Date deathDate = (java.sql.Date) row.row.get(2).getValue().cast(row.row.get(2).getKey()); 
+			String wikiPageID = (String) row.row.get(3).getValue().cast(row.row.get(3).getKey());
+			res.add(new TableEntry(wikiURL + wikiPageID, name, "", "", birthDate, deathDate, occupation, "", ""));
+		}
+		return res;
+	}
+
+	public ArrayList<TableEntry> getSpouselessBetweenYears(String year1, String year2) throws SQLException, ClassNotFoundException, IOException, ParseException {
+		logger.log(Level.INFO, "spouseless between years is being executed");
+		final String spouselessBetweenYears = "SELECT SQL_CACHE name,birthDate,deathDate,occupation,wikiPageId "
+				+ "FROM basic_info, WikiID "
+				+ "WHERE deathDate IS NOT NULL "
+				+ "AND YEAR(birthDate) >= ? AND YEAR(deathDate) <= ? "
+				+ "AND spouseName = 'No Spouse Name' "
+				+ "AND occupation = ? "				
+				+ "AND wikiPageID = (SELECT wikiPageID FROM WikiID WHERE WikiID.name = basic_info.name LIMIT 1)"; 
+		Object[] inp = new Object[] { year1, year2 };
+		ArrayList<Row> rs = conn.runQuery(spouselessBetweenYears,inp); 
+		ArrayList<TableEntry> res = new ArrayList<TableEntry>();
+		for (Row row : rs){
+			String name = (String) row.row.get(0).getValue().cast(row.row.get(0).getKey()); 
+			Date birthDate = (java.sql.Date) row.row.get(1).getValue().cast(row.row.get(1).getKey()); 
+			Date deathDate = (java.sql.Date) row.row.get(2).getValue().cast(row.row.get(2).getKey()); 
+			String occupation = (String) row.row.get(3).getValue().cast(row.row.get(3).getKey()); 			
+			String wikiPageID = (String) row.row.get(4).getValue().cast(row.row.get(4).getKey());
+			res.add(new TableEntry(wikiURL + wikiPageID, name, "", "", birthDate, deathDate, occupation, "", ""));
+		}
+		return res;
+	}	
+	
+	public ArrayList<Row> runQuery(String s,Object[] inp) throws ClassNotFoundException, SQLException, IOException{
+		return conn.runQuery(s,inp);
+	}
 }
--- src/infoeval/test/mysqlTest/SqlRunnerTest.java
+++ src/infoeval/test/mysqlTest/SqlRunnerTest.java
@@ -1,13 +1,20 @@
 package infoeval.test.mysqlTest;
 
 import infoeval.main.mysql.SqlRunner;
+import infoeval.main.mysql.Row;
 import infoeval.main.mysql.TableEntry;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.Ignore;
+import org.junit.BeforeClass;
+import org.junit.AfterClass;
 
+import java.io.IOException;
+import java.sql.SQLException;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,60 +25,86 @@ import java.util.List;
  *
  */
 public class SqlRunnerTest {
-	SqlRunner querun;
-	
-	/**
-	 *
-	 * @throws Exception 
-	 */
-	
+	static SqlRunner querun;
 
-	
+	/**
+	 * [[SuppressWarningsSpartan]]
+	 * 
+	 * @throws Exception
+	 */
+
+	@BeforeClass
+	public static void initRunner() throws Exception {
+		querun = new SqlRunner();
+	}
+
+	@AfterClass
+	public static void close() {
+		querun.close();
+	}
+
+	// @Ignore
 	@Test
 	public void getBornInPlaceBeforeYearTest() throws Exception {
-		SqlRunner querun = new SqlRunner();
-		List<TableEntry> lst = (querun.getBornInPlaceBeforeYear("Frankfurt", "1900"));
-		for (TableEntry te : lst) {
+		for (TableEntry te : querun.getBornInPlaceBeforeYear("Casablanca", "1954")) {
 			java.sql.Date birthDate = te.getBirthDate();
 			String birthPlace = te.getBirthPlace();
 			SimpleDateFormat formatYear = new SimpleDateFormat("yyyy");
 			String birthYear = formatYear.format(birthDate);
 			int bYear = Integer.parseInt(birthYear);
-			assert bYear < 1900;
-			assertEquals("Frankfurt",birthPlace);
+			assert bYear < 1954;
+			assertEquals("Casablanca", birthPlace);
 		}
-		querun.close();
 	}
 
-	
-	
-//	@Test
-//	public void getDifferentDeathPlaceTest() throws Exception {
-//		SqlRunner querun = new SqlRunner();
-//		List<TableEntry> lst = (querun.getDifferentDeathPlace());
-//		for (TableEntry ¢ : lst) {
-//			assertNotEquals(¢.getBirthPlace(), ¢.getDeathPlace());
-//			System.out.println("Birth place:"+¢.getBirthPlace()+" Death place:"+¢.getDeathPlace());
-//		}
-//		querun.close();
-//	}
-	
-/*	@Ignore
+	@Ignore
 	@Test
-	public getSameOccupationCouplesTest() throws Exception{
-		SqlRunner querun = new SqlRunner();
-		List<TableEntry> lst = querun.getSameOccupationCouples();
-		for (TableEntry te : lst) {
-			
-			//java.sql.Date birthDate = te.getBirthDate();
-			//String birthPlace = te.getBirthPlace();
-			//SimpleDateFormat formatYear = new SimpleDateFormat("yyyy");
-			//String birthYear = formatYear.format(birthDate);
-			//int bYear = Integer.parseInt(birthYear);
-			//assert bYear < 1954;
-			//assertEquals("Casablanca",birthPlace);
-		}
-	querun.close();
-	}*/
-}
+	public void getDifferentDeathPlaceTest() throws Exception {
+		for (TableEntry ¢ : querun.getDifferentDeathPlace())
+			assertNotEquals(¢.getBirthPlace(), ¢.getDeathPlace());
+	}
 
+	@Ignore
+	@Test
+	public void getSameOccupationCouplesTest() throws Exception {
+		for (TableEntry te : querun.getSameOccupationCouples())
+			assertEquals(te.getOccupation(), te.getSpouseOccupation());
+	}
+
+	@Ignore
+	@Test
+	public void getSameBirthPlaceCouplesTest() throws Exception {
+		for (TableEntry te : querun.getSameBirthPlaceCouples()) {
+			String sName = te.getSpouseName(),
+					checkBirthPlaceQuery = "SELECT birthPlace FROM basic_info WHERE name=? LIMIT 1";
+			Object[] inp = new Object[1];
+			inp[0] = sName;
+			ArrayList<Row> res = querun.runQuery(checkBirthPlaceQuery, inp);
+			Row row = res.get(0);
+			assertEquals(te.getBirthPlace(), (String) row.row.get(0).getValue().cast(row.row.get(0).getKey()));
+		}
+	}
+
+	@Ignore
+	@Test
+	public void getOccupationBetweenYearsTest() throws Exception {
+		for (TableEntry te : querun.getOccupationBetweenYears("1840", "1920", "Politician")) {
+			assertEquals(te.getOccupation(), "Politician");
+			java.sql.Date birthDate = te.getBirthDate();
+			SimpleDateFormat formatYear = new SimpleDateFormat("yyyy");
+			String birthYear = formatYear.format(birthDate);
+			int bYear = Integer.parseInt(birthYear);
+			assert bYear >= 1840;
+			java.sql.Date deathDate = te.getDeathDate();
+			formatYear.format(deathDate);
+			assert Integer.parseInt(birthYear) <= 1920;
+		}
+	}
+
+	@Ignore
+	@Test
+	public void getSpouselessBetweenYearsTest() throws Exception {
+		for (TableEntry te : querun.getSpouselessBetweenYears("1900", "1980"))
+			assertEquals(te.getSpouseName(), "No Spouse Name");
+	}
+}
