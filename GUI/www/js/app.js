d4ff02d6caf99cc7c513998eb6d89cbd393bb1f5
MosheEliasof <moshikoe@campus.technion.ac.il>
2017-05-26 09:15:27 -0700

    Front page is correct now #183

--- GUI/www/js/app.js
+++ GUI/www/js/app.js
@@ -199,5 +199,5 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
     ;
 
     // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise('/app/login');
+    $urlRouterProvider.otherwise('/app/Search');
 });
