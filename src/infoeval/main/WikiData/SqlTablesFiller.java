926ed8df53cbcf9cceb6a2f1807497664aa64146
NetanelFelcher <netanel_felcher@hotmail.com>
2017-06-16 13:31:37 +0300

    trying to fix spouse name problem #248

--- src/infoeval/main/WikiData/SqlTablesFiller.java
+++ src/infoeval/main/WikiData/SqlTablesFiller.java
@@ -88,8 +88,12 @@ public class SqlTablesFiller {
 		QuerySolution solution = r.nextSolution();
 		String name = solution.getLiteral("name").getString();
 
-		RDFNode sName = solution.get("sname");
-		String spouseName = sName == null || !sName.isLiteral() ? "No Spouse" : sName.asLiteral().getString() + "";
+		RDFNode sName = solution.getLiteral("sname");
+		String spouseName="No Spouse";
+		if(sName!=null){
+			spouseName=solution.getLiteral("sname").getString();
+		}
+		
 		RDFNode bPlace = solution.get("birth");
 		String birthPlace = "No Birth Place";
 		if (bPlace != null)
